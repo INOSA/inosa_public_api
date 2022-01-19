fbdaad6d1a1eaf0d203a0a4a86f6b4aad2b63d04
mateuszkoziol <78360242+mateuszkoziol@users.noreply.github.com>
2022-01-19 15:54:55 +0100

    feat(public): setup on container initialization (#28)

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env sh
+
 set -eux
 
-composer install
-bin/console doctrine:migrations:migrate -n --allow-no-migration --all-or-nothing --no-debug
+composer clear-cache
+
+if [ "${ENV}" == "dev" ] || [ ${ENV} == "test" ]
+then
+  echo "Installing dev/test dependencies"
+
+  composer install --no-cache
+  composer dump-autoload
+
+  bin/console doctrine:migrations:migrate -n
+  bin/console doctrine:fixtures:load -n
+else
+  composer dump-autoload --no-dev --optimize
+  bin/console doctrine:migrations:migrate -n
+fi
+

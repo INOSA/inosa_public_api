844698aa744dd9c71d9f5e169ea9204c51599016
mateuszkoziol <78360242+mateuszkoziol@users.noreply.github.com>
2022-03-14 13:10:53 +0100

    feat(public): nso-10594 (#48)

---
+++ src/AuthorizationServer/ConnectUserToRoles/UI/ConnectUserToRolesController.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\AuthorizationServer\ConnectUserToRoles\UI;
+
+use App\AuthorizationServer\ConnectUserToRoles\Application\Command\ConnectUserToRolesCommand;
+use App\AuthorizationServer\ConnectUserToRoles\Application\ConnectUserToRolesRequest;
+use App\Shared\Application\CommandHandlerException;
+use App\Shared\Application\Json\JsonDecoderInterface;
+use App\Shared\Application\MessageBus\MessageBusInterface;
+use App\Shared\Domain\Identifier\IdentifierFactoryInterface;
+use App\Shared\Domain\Identifier\UserIdentifier;
+use App\Shared\Infrastructure\Controller\ClientCredentialsAuthorizationController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+
+final class ConnectUserToRolesController extends ClientCredentialsAuthorizationController
+{
+    public function connectUserToRoles(
+        Request $request,
+        DenormalizerInterface $serializer,
+        JsonDecoderInterface $jsonDecoder,
+        MessageBusInterface $messageBus,
+        IdentifierFactoryInterface $identifierFactory,
+    ): JsonResponse {
+        /** @var ConnectUserToRolesRequest $connectUserToRolesRequest */
+        $connectUserToRolesRequest = $serializer->denormalize(
+            [
+                'userId' => $request->get('userId'),
+                'content' => $jsonDecoder->decode($request->getContent())->toArray(),
+            ],
+            ConnectUserToRolesRequest::class,
+        );
+
+        try {
+            $messageBus->dispatch(
+                new ConnectUserToRolesCommand(
+                    UserIdentifier::fromIdentifier($identifierFactory->fromString($connectUserToRolesRequest->userId)),
+                    $connectUserToRolesRequest->getRolesAsList(),
+                )
+            );
+
+            return new JsonResponse(null, 204);
+        } catch (CommandHandlerException $e) {
+            return new JsonResponse(['data' => [], 'error' => $e->getMessage()], $e->getCode());
+        }
+    }
+}
